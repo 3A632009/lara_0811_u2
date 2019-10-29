339c1cbb85ddc27fcdb2c9c91b1c639026ebcf94
Sue Qing Ya <3a632009@student.ncut.edu.tw>
2019-10-30 00:09:04 +0800

    練習五 設定 Route 前置

--- routes/web.php
+++ routes/web.php
@@ -37,8 +37,18 @@ Route::get('hello/{name?}',function($name='Everybody'){
 });
 
 //4.1將 Route 取名為 hello.index
-Route::get('hello/{name?}',['as'=>'hello.index',function($name=
-                                                         'Everybody'){
+Route::get('hello/{name?}',['as'=>'hello.index',function($name='Everybody'){
     return'Hello,'.$name;
 }]);
 
+//5.1設定 dashboard路徑的 Route
+Route::get('dashboard',function(){
+    return'dashboard';
+});
+
+//5.2設定另一個 Route 以群組包起來設定 prefix
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('dashboard',function(){
+        return'admindashboard';
+    });
+});
